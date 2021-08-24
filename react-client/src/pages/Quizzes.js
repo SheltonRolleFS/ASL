9cbb2eda31425b87d51a67e964bed5aa47516504
Shelton Rolle <sheltonrolle@Sheltons-MacBook-Pro-2.local>
2021-08-24 01:56:45 -0400

    Added SASS and base SASS files such as a reset file, a variables file, a base file and a main file to contain all of the sass styling files.

--- react-client/src/pages/Quizzes.js
+++ react-client/src/pages/Quizzes.js
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+
+// Component Imports
 import Header from '../components/Header'
+import Row from '../components/Row'
 
 function Quizzes() {
+    const [quizzes, setQuizzes] = useState([])
 
-    const fetchData = async () => {
+    useEffect(() => {
         const opts = {
             method: 'GET',
             headers: {
@@ -13,19 +18,35 @@ function Quizzes() {
         fetch('http://localhost:3001/quizzes', opts)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            setQuizzes(data)
         })
-    }
-
-    useEffect(() => {
-        fetchData()
     }, [])
 
     
     return (
         <>
             <Header />
-            <h1>Quizzes</h1>
+
+            <section className="content">
+                <div className="heading">
+                    <div className="heading__text">
+                        <h1>Quizzes</h1>
+                        <p>Here are all the quizzes in the API</p>
+                    </div>
+                    <Link to="/quizzes/new">Create Quiz</Link>
+                </div>
+
+                <div className="table-header">
+                    <h2>Quiz Name</h2>
+                    <h2>Weight</h2>
+                    <h2>ID</h2>
+                    <h2>No. of Questions</h2>
+                    <h2>Date Created</h2>
+                </div>
+                {quizzes.map((q) => {
+                    return <Row key={q.id} quiz={q}/>
+                })}
+            </section>
         </>
     )
 }
